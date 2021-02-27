a269f267cce543a87b68fedc4ea346bb8e6bed2b
trevismurithi <trevismurithi@gmail.com>
2021-02-27 23:57:31 +0300

    new project

---
+++ header.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/header.css">
+    <link rel="stylesheet" href="css/index.css">
+    <link rel="stylesheet" href="css/benefits.css">
+    <link rel="stylesheet" href="css/entail.css">
+    <link rel="stylesheet" href="css/services.css">
+    <link rel="stylesheet" href="css/testimonials.css">
+    <link rel="stylesheet" href="css/join.css">
+    <title>Essay</title>
+</head>
+<body>
+    <nav>
+        <div class="nav-contact">
+            <ul>
+                <li><h2>TURN YOUR SKILLS INTO MONEY</h2></li>
+                <li>Phone 254-724462514</li>
+                <li>Email trevismurithi@gmail.com</li>
+            </ul>
+        </div>
+        <div class="nav-links">
+            <img src="img/ignite.png" width="92vh" alt="ignite-icon">
+            <ul>
+                <li><a href="#">How it works</a></li>
+                <li><a href="#">Get started</a></li>
+                <li><a href="#">About</a></li>
+                <li><a href="#">FAQ</a></li>
+                <li><a href="#">Contact Us</a></li>
+            </ul>
+            <div class="user-accounts">
+                <li><a class="user-accounts-login" href="#">login</a></li>
+                <li><a class="user-accounts-sign-up" href="#">sign up</a></li>
+            </div>
+        </div>
+    </nav>
---
+++ index.php
@@ -0,0 +1,143 @@
+<?php require 'header.php';?>
+
+<main>
+    <div class="container">
+        <div class="image-content">
+            <img src="img/cover_banner.jpg" alt="">
+            <div class="image-context">
+                <div class="image-details">
+                    <p><span>Writing service</span> at your own convinience</p>
+                    <h1>Hire Your Personal Essay Writer Today</h1><br>
+                    <h5>Essay writing service for students who want to see results twice as fast.</h5><br>
+                    <p><a href="#">Write My Paper</a></p>
+                </div>
+                <form class="calculator-form" action="" method="post">
+                    <h3>calculate the price</h3>
+                    <div class="form-btn-3">
+                        <input type="button" value="Writing">
+                        <input type="button" value="Rewriting">
+                        <input type="button" value="Editing">
+                    </div>
+                    <div class="form-select-1">
+                        <select name="article" id="article">
+                            <option value="Essay">Essay</option>
+                            <option value="Admission">Admission</option>
+                        </select>
+                    </div>
+                    <div class="form-select-2">
+                        <select name="university" id="university">
+                            <option value="school">school</option>
+                            <option value="college">college</option>
+                        </select>
+
+                        <select name="Week" id="Week">
+                            <option value="1">1 week</option>
+                            <option value="2">2 week</option>
+                        </select>
+                    </div>
+                    <div class="form-select-1">
+                        <select name="article" id="article">
+                            <option value="Essay">Essay</option>
+                            <option value="Admission">Admission</option>
+                        </select>
+                    </div>
+                    <div class="form-check-2">
+                        <label for="double">Double spaces</label>
+                        <input type="radio" name="double" id="double">
+                        <label for="single">Single spaces</label>
+                        <input type="radio" name="single" id="single">
+                    </div>
+                    <button class="btn-submit" name="write-paper" type="submit">Write My Paper</button>
+                </form>
+            </div>
+        </div>
+        <div class="benefits">
+            <h2>Benefits</h2>
+            <img src="img/graph.png" alt="">
+        </div>
+        <div class="entail">
+            <ul>
+                <li><img src="img/benefit1.png" alt=""></li>
+                <li>No Registration/<br>Sign up fees</li>
+            </ul>
+            <ul>
+                <li><img src="img/benefit2.png" alt=""></li>
+                <li>24/7 Responsive <br>Support Team</li>
+            </ul>
+            <ul>
+                <li><img src="img/benefit3.png" alt=""></li>
+                <li>Great Mentoring<br>Program</li>
+            </ul>
+            <ul>
+                <li><img src="img/benefit4.png" alt=""></li>
+                <li>Regular and Secure <br>Payment Processing</li>
+            </ul>
+            <ul>
+                <li><img src="img/benefit5.png" alt=""></li>
+                <li>Best Pay Rates <br>in the Business</li>
+            </ul>
+        </div>
+        <div class="services">
+            <h2>Current Available</h2>
+        </div>
+        <div class="available-services">
+            <div class="projects">
+                <div class="single">
+                    <p><span>Type of paper:</span> Lifestyle Article</p>
+                    <p><span>Topic: </span>Fashion Race</p>
+                    <p><span>Due Date: </span>March 5</p>
+                    <div class="horizontal"></div>
+                    <p><span>Price: </span><span>$35</span></p>
+                </div>
+                <div class="single">
+                    <p><span>Type of paper:</span> Lifestyle Article</p>
+                    <p><span>Topic: </span>Fashion Race</p>
+                    <p><span>Due Date: </span>March 5</p>
+                    <div class="horizontal"></div>
+                    <p><span>Price: </span><span>$35</span></p>
+                </div>
+                <div class="single">
+                    <p><span>Type of paper:</span> Lifestyle Article</p>
+                    <p><span>Topic: </span>Fashion Race</p>
+                    <p><span>Due Date: </span>March 5</p>
+                    <div class="horizontal"></div>
+                    <p><span>Price: </span><span>$35</span></p>
+                </div>
+                <div class="single">
+                    <p><span>Type of paper:</span> Lifestyle Article</p>
+                    <p><span>Topic: </span>Fashion Race</p>
+                    <p><span>Due Date: </span>March 5</p>
+                    <div class="horizontal"></div>
+                    <p><span>Price: </span><span>$35</span></p>
+                </div>
+            </div>
+            <div class="view-projects">
+                <a href="#">check all projects</a>
+            </div>
+            <br>
+        </div>
+        <div class="testimonials">
+            <h2>Testimonials</h2>
+        </div>
+        <div class="beneficiary">
+            <img src="img/dwayne.png" alt="">
+            <div class="beneficiary-details">
+                <h5>Dwayne Johnson</h6>
+                <p><span>Joined in April 2016</span></p>
+                <br>
+                <p>I have been using WritersHub since 2016, 
+                    and I have to say that it is a very useful platform for those who want to transform skills into earning. 
+                    I would like to express my sincere gratitude to the QA team for their professional guidance 
+                    and Support Department for their 24/7 help. Also, I am espe...
+                </p>
+                <br>
+                <a href="#">Show more</a>
+            </div>
+        </div>
+        <div class="join-us"><br>
+            <a href="#">Join Us</a><br>
+        </div><br>
+    </div>
+</main>
+
+<?php require 'footer.php';?>
