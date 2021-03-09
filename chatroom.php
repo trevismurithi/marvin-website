7bb69c203f73fae3339daf03865627c4d6100c1c
trevismurithi <trevismurithi@gmail.com>
2021-03-10 00:55:16 +0300

    chat display

--- chatroom.php
+++ chatroom.php
@@ -8,24 +8,67 @@
             </div>
             <div class="default-setting">
                 <ul>
-                    <li><img src="img/write.png" alt=""><p>Country</p></li>
-                    <li><img src="img/write.png" alt=""><p>Email</p></li>
-                    <li><img src="img/write.png" alt=""><p>Phone</p></li>
-                    <li><img src="img/write.png" alt=""><p>Wallet</p></li>
-                    <li><img src="img/write.png" alt=""><p>Write</p></li>
+                    <li><img src="img/write.png" alt=""><p>Writing is a form of art</p></li>
+                    <li><img src="img/write.png" alt=""><p>trevismurithi@gmail.com</p></li>
+                    <li><img src="img/write.png" alt=""><p>+254-724-462-514</p></li>
                 </ul>
             </div>
         </div>
         <div class="connect">
             <div class="choose-chat">
-                <h4>Chat</h4>
+                <h2>Chat</h2>
                 <div class="user-chat">
-                    <div class="admin">
-                        
+                    <div class="user-list">
+                        <div class="one-user">
+                            <div class="user-status">
+                                <img src="img/admin.jpg" alt="">
+                                <div class="online"></div>
+                            </div>
+                            <div class="user-content">
+                                <h6>Carlos Santino(Admin)</h6>
+                                <p>Hello and welcome feel..</p>
+                            </div>
+                            <div class="user-time">
+                                <p>3 March</p>
+                                <button>reply</button>
+                            </div>
+                        </div>
+
+                        <div class="one-user">
+                            <div class="user-status">
+                                <img src="img/writer.jpg" alt="">
+                                <div class="online"></div>
+                            </div>
+                            <div class="user-content">
+                                <h6>Paul John(Writer)</h6>
+                                <p>I am here at your service..</p>
+                            </div>
+                            <div class="user-time">
+                                <p>3 March</p>
+                                <button>reply</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-            <div class="chat"></div>
+            <div class="chat">
+                <div class="current-user">
+                    <div class="user-status-chat">
+                        <img src="img/writer.jpg" alt="">
+                        <div class="online"></div>
+                    </div>
+                    <h6>Paul John(Writer)</h6>
+                </div>
+                <div class="chat-content">
+                    <div class="seller">
+                        <div class="receive"><img src="img/writer.jpg" alt=""><p>Hello Detroit</p></div>
+                    </div>
+                    <div class="buyer">
+                        <div></div>
+                        <div class="sent"><img src="img/dwayne.png" alt=""><p>Hello John</p></div>
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 </main>
